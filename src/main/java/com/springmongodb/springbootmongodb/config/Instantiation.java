4ccf4456588e9507c4a4b50cbc21b8c4434b7658
Raphael Feitosa <raphaelcs2@gmail.com>
2021-04-27 18:56:38 -0300

    Creating entity Post with nested User

--- src/main/java/com/springmongodb/springbootmongodb/config/Instantiation.java
+++ src/main/java/com/springmongodb/springbootmongodb/config/Instantiation.java
@@ -1,31 +1,47 @@
 package com.springmongodb.springbootmongodb.config;
 
+import com.springmongodb.springbootmongodb.domain.Post;
 import com.springmongodb.springbootmongodb.domain.User;
+import com.springmongodb.springbootmongodb.repository.PostRepository;
 import com.springmongodb.springbootmongodb.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 @Configuration
 public class Instantiation  implements CommandLineRunner {
 
     private final UserRepository  userRepository;
+    private final PostRepository  postRepository;
 
-    public Instantiation(UserRepository userRepository) {
+
+    public Instantiation(UserRepository  userRepository, PostRepository postRepository) {
         this.userRepository = userRepository;
+        this.postRepository = postRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
 
-        userRepository.deleteAll();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
+        this.userRepository.deleteAll();
+        this.postRepository.deleteAll();
 
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+        Post post1 = new Post(null, sdf.parse("21/03/2021"), "Partiu viagem",
+                "Vou viajar para São Paulo!", maria);
+        Post post2 = new Post(null, sdf.parse("21/03/2021"), "Bom dia",
+                "Acordei feliz hoje!", maria);
+
+        this.userRepository.saveAll(Arrays.asList(maria, alex, bob));
+        this.postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
--- src/main/java/com/springmongodb/springbootmongodb/repository/PostRepository.java
+++ src/main/java/com/springmongodb/springbootmongodb/repository/PostRepository.java
@@ -1,10 +1,10 @@
 package com.springmongodb.springbootmongodb.repository;
 
-import com.springmongodb.springbootmongodb.domain.User;
+import com.springmongodb.springbootmongodb.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, String> {
+public interface PostRepository extends MongoRepository<Post, String> {
 
 }
