50f44583df686abf793de11b8b18b2e1724eae5b
Raphael Feitosa <raphaelcs2@gmail.com>
2021-04-26 22:43:01 -0300

    Getting user by id

--- src/main/java/com/springmongodb/springbootmongodb/resources/UserResouce.java
+++ src/main/java/com/springmongodb/springbootmongodb/resources/UserResouce.java
@@ -4,6 +4,7 @@ import com.springmongodb.springbootmongodb.domain.User;
 import com.springmongodb.springbootmongodb.dto.UserDTO;
 import com.springmongodb.springbootmongodb.service.UserService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,8 +25,15 @@ public class UserResouce {
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<List<UserDTO>> findAll(){
 
-        List<User> list = userService.findAll();
+        List<User> list = this.userService.findAll();
         List<UserDTO> listDto = list.stream().map( x -> new UserDTO(x)).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDto);
     }
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<UserDTO> findById(@PathVariable String id){
+
+        User user = this.userService.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(user));
+    }
+
 }
--- src/main/java/com/springmongodb/springbootmongodb/service/UserService.java
+++ src/main/java/com/springmongodb/springbootmongodb/service/UserService.java
@@ -1,9 +1,11 @@
 package com.springmongodb.springbootmongodb.service;
 import com.springmongodb.springbootmongodb.domain.User;
 import com.springmongodb.springbootmongodb.repository.UserRepository;
+import com.springmongodb.springbootmongodb.service.exception.ObjectNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,6 +17,11 @@ public class UserService {
     }
 
     public List<User> findAll() {
-        return userRepository.findAll();
+        return this.userRepository.findAll();
+    }
+
+    public User findById(String id) {
+            Optional<User> user = this.userRepository.findById(id);
+            return user.orElseThrow(() -> new ObjectNotFoundException("User not Found"));
     }
 }
