9ed975f115b1826081c92d6cadc4f581c6110b26
Raphael Feitosa <raphaelcs2@gmail.com>
2021-04-26 21:34:44 -0300

    Database instantiation operation

---
+++ src/main/java/com/springmongodb/springbootmongodb/config/Instantiation.java
@@ -0,0 +1,31 @@
+package com.springmongodb.springbootmongodb.config;
+
+import com.springmongodb.springbootmongodb.domain.User;
+import com.springmongodb.springbootmongodb.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Instantiation  implements CommandLineRunner {
+
+    private final UserRepository  userRepository;
+
+    public Instantiation(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        userRepository.deleteAll();
+
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+    }
+}
