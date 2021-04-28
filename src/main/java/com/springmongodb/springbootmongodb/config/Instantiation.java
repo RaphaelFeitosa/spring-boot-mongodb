54745db39ed5eb2cfd533e4a7cdbc81950893cde
Raphael Feitosa <raphaelcs2@gmail.com>
2021-04-27 22:43:48 -0300

    Adding comments to posts

--- src/main/java/com/springmongodb/springbootmongodb/config/Instantiation.java
+++ src/main/java/com/springmongodb/springbootmongodb/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.springmongodb.springbootmongodb.config;
 import com.springmongodb.springbootmongodb.domain.Post;
 import com.springmongodb.springbootmongodb.domain.User;
 import com.springmongodb.springbootmongodb.dto.AuthorDTO;
+import com.springmongodb.springbootmongodb.dto.CommentDTO;
 import com.springmongodb.springbootmongodb.repository.PostRepository;
 import com.springmongodb.springbootmongodb.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
@@ -18,8 +19,7 @@ public class Instantiation  implements CommandLineRunner {
     private final UserRepository  userRepository;
     private final PostRepository  postRepository;
 
-
-    public Instantiation(UserRepository  userRepository, PostRepository postRepository) {
+    public Instantiation(UserRepository userRepository, PostRepository postRepository) {
         this.userRepository = userRepository;
         this.postRepository = postRepository;
     }
@@ -43,6 +43,13 @@ public class Instantiation  implements CommandLineRunner {
                 "Vou viajar para São Paulo!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("21/03/2021"), "Bom dia",
                 "Acordei feliz hoje!", new AuthorDTO(maria));
+
+        CommentDTO c1 = new CommentDTO("Boa viagem mano", sdf.parse("21/03/2020"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("21/03/2020"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um ótimo dia", sdf.parse("21/03/2020"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1, c2));
+        post2.getComments().addAll(Arrays.asList(c3));
 
         this.postRepository.saveAll(Arrays.asList(post1, post2));
 
--- src/main/java/com/springmongodb/springbootmongodb/domain/Post.java
+++ src/main/java/com/springmongodb/springbootmongodb/domain/Post.java
@@ -1,12 +1,16 @@
 package com.springmongodb.springbootmongodb.domain;
 
 import com.springmongodb.springbootmongodb.dto.AuthorDTO;
+import com.springmongodb.springbootmongodb.dto.CommentDTO;
 import com.sun.source.doctree.AuthorTree;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Document
 public class Post  implements Serializable {
@@ -19,6 +23,8 @@ public class Post  implements Serializable {
     private String title;
     private String body;
     private AuthorDTO author;
+
+    private List<CommentDTO> comments = new ArrayList<>();
 
     public Post() {
 
@@ -70,6 +76,14 @@ public class Post  implements Serializable {
 
     public void setAuthor(AuthorDTO author) {
         this.author = author;
+    }
+
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
     }
 
     @Override
